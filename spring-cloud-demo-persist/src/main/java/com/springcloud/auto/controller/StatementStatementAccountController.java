4a1d5d1b3bae7d79bf3dcdb059e0214c2fd1da48
zhanglong <718168633@qq.com>
2019-07-13 19:57:09 +0800

    修改实体

---
+++ spring-cloud-demo-persist/src/main/java/com/springcloud/auto/controller/StatementStatementAccountController.java
@@ -0,0 +1,21 @@
+package com.springcloud.auto.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.springcloud.common.BaseController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zhanglong
+ * @since 2019-07-12
+ */
+@RestController
+@RequestMapping("/auto/statement-statement-account")
+public class StatementStatementAccountController extends BaseController {
+
+}
