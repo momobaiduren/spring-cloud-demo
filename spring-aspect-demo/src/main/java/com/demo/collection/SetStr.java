515f0f6da4ad7747f6665c8e7e5fc9d1ea8cb6cd
zhanglong <718168633@qq.com>
2019-07-23 19:32:37 +0800

    java基础测试

---
+++ spring-aspect-demo/src/main/java/com/demo/collection/SetStr.java
@@ -0,0 +1,64 @@
+package com.demo.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @title: SetStr
+ * @projectName spring-cloud-demo
+ * @description: Set不重复
+ * @author zhanglong
+ * @date 2019-07-2123:28
+ */
+
+public class SetStr {
+
+  public static void main(String[] args) {
+    setStr();
+    setIntegerAndInt();
+  }
+  public static void setStr(){
+    Set<String> setStr1 = new HashSet<>();
+    String s1 = "sss";
+    String s2 = "sss";
+    String s3 = "aaa";
+    String s4 = "bbb";
+    setStr1.add(s1);
+    setStr1.add(s2);
+    setStr1.add(s3);
+    setStr1.add(s4);
+    System.out.println(s1.equals(s2));
+    System.out.println(setStr1);
+    System.out.println("-------------------------");
+    Set<String> setStr2 = new HashSet<>();
+    setStr2.add("sss");
+    setStr2.add("sss");
+    setStr2.add("aaa");
+    setStr2.add("bbb");
+    System.out.println(setStr2);
+  }
+
+  public static void setIntegerAndInt(){
+    Set<Integer> setInteger = new HashSet<>();
+    Integer i1 = 1;
+    Integer i2 = 1;
+    Integer i3 = 2;
+    Integer i4 = 3;
+    setInteger.add(i1);
+    setInteger.add(i2);
+    setInteger.add(i3);
+    setInteger.add(i4);
+    System.out.println(i1.equals(i2));
+    System.out.println(setInteger);
+    System.out.println("-------------------------");
+    Set<Integer> setInt = new HashSet<>();
+    setInt.add(1);
+    setInt.add(1);
+    setInt.add(2);
+    setInt.add(3);
+    System.out.println(setInt);
+  }
+
+
+
+}
